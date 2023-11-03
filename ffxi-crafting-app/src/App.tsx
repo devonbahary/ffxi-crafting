4426252376bdf7897871b79b5fa87ff611d1f429
Devon Bahary <devonbahary@gmail.com>
2023-11-03 13:10:19 -0700

    adds MUI, basic app toolbar and drawer

--- ffxi-crafting-app/src/App.tsx
+++ ffxi-crafting-app/src/App.tsx
@@ -1,7 +1,187 @@
-import React from 'react';
+import React, { useState, Fragment, ReactNode } from 'react';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CarpenterIcon from '@mui/icons-material/Carpenter';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import CssBaseline from '@mui/material/CssBaseline';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import MuiDrawer from '@mui/material/Drawer';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import {
+    CSSObject,
+    Theme,
+    ThemeProvider,
+    createTheme,
+    styled,
+    useTheme,
+} from '@mui/material/styles';
 
-function App() {
-    return <div>Hello World</div>;
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const closedMixin = (theme: Theme): CSSObject => ({
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+        width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+});
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean;
 }
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+    shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+    boxSizing: 'border-box',
+    ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+    ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+    }),
+}));
+
+interface MenuItem {
+    text: string;
+    icon: ReactNode;
+}
+
+const menuItems: MenuItem[] = [
+    {
+        text: 'Dashboard',
+        icon: <DashboardIcon />,
+    },
+    {
+        text: 'Auction House',
+        icon: <StorefrontIcon />,
+    },
+    {
+        text: 'Crafts',
+        icon: <CarpenterIcon />,
+    },
+];
+
+const App = () => {
+    const [open, setOpen] = useState(true);
+
+    const theme = useTheme();
+
+    const toggleDrawer = () => setOpen(!open);
+
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar position="fixed" open={open}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            onClick={toggleDrawer}
+                            edge="start"
+                            sx={{
+                                marginRight: 5,
+                                ...(open && { display: 'none' }),
+                            }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" noWrap component="div">
+                            FFXI Crafting
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Drawer variant="permanent" open={open}>
+                    <DrawerHeader>
+                        <IconButton onClick={toggleDrawer}>
+                            {theme.direction === 'rtl' ? (
+                                <ChevronRightIcon />
+                            ) : (
+                                <ChevronLeftIcon />
+                            )}
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    <List>
+                        {menuItems.map(({ text, icon }) => (
+                            <Fragment key={text}>
+                                <ListItem>
+                                    <ListItemButton>
+                                        <ListItemIcon>{icon}</ListItemIcon>
+                                        <ListItemText>{text}</ListItemText>
+                                    </ListItemButton>
+                                </ListItem>
+                                <Divider />
+                            </Fragment>
+                        ))}
+                    </List>
+                </Drawer>
+            </Box>
+        </ThemeProvider>
+    );
+};
 
 export default App;
